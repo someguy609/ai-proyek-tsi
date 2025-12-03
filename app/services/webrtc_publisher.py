77886c5bac52960be0e07c4fd5e4e75ccb2a1485
Daniel Adhitthana <dadhitthana@gmail.com>
2025-12-04 01:39:39 +0700

    refactor: frame format

--- app/services/webrtc_publisher.py
+++ app/services/webrtc_publisher.py
@@ -1,8 +1,18 @@
 import asyncio
+import numpy as np
+import cv2
 import multiprocessing as mp
 
 from app.media.tracks import AnnotatedFrameTrack
 
+def _get_and_decode(frame_queue: mp.Queue, timeout: float = 0.1):
+    try:
+        frame_bytes = frame_queue.get(timeout=timeout)
+        arr = np.frombuffer(frame_bytes, dtype=np.uint8)
+        img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+        return img
+    except Exception as e:
+        raise
 
 async def webrtc_publisher(
         camera_id: int,
@@ -13,8 +23,10 @@ async def webrtc_publisher(
     try:
         while True:
             try:
-                annotated_frame = await asyncio.to_thread(frame_queue.get, timeout=0.1)
-            except:
+                annotated_frame = await asyncio.to_thread(_get_and_decode, frame_queue, timeout=0.1)
+            except asyncio.CancelledError:
+                raise asyncio.CancelledError()
+            except Exception as e:
                 await asyncio.sleep(0.001)
                 continue
             await track.update(annotated_frame)
